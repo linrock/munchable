906b4179c0ad8921874f02eb4bd5cf1501590060
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-22 17:13:52 -0700

    Add user_session model and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  filter_parameter_logging :password
+  helper_method :current_user
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
+  end
+
+private
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
   end
 end
