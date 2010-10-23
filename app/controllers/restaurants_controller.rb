051fd830d9de680cbddc6137302d42ad6d6c61a2
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-22 19:15:53 -0700

    Use to_json instead of serializer

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,13 @@
 class RestaurantsController < ApplicationController
 
   def index
-    serializer = ActiveSupport::JSON
     @restaurants = Restaurant.find(:all, :limit => 30)
-    @restaurants_serialized = serializer.encode(@restaurants)
   end
 
   def show
     id = params[:id]
     @restaurant = Restaurant.find(id)
     @comments = MenuComment.where(:restaurant_id => id)
-    @restaurant_serialized = ActiveSupport::JSON.encode([@restaurant])
     @coordinates = [@restaurant[:x], @restaurant[:y]]
   end
 end
