48e304a4fefe41e53cc4db1880f912f61daeee5b
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-22 17:39:43 -0700

    Add loading.gif for waiting for Google maps to load

--- db/schema.rb
+++ db/schema.rb
@@ -53,21 +53,27 @@ ActiveRecord::Schema.define(:version => 0) do
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
-    t.string   "reset_password_token"
-    t.string   "remember_token"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "school_id"
-    t.string   "role"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    :login,               :null => false   # optional, you can use email instead, or both
+    t.string    :email,               :null => false   # optional, you can use login instead, or both
+    t.string    :crypted_password,    :null => false   # optional, see below
+    t.string    :password_salt,       :null => false   # optional, but highly recommended
+    t.string    :persistence_token,   :null => false   # required
+    t.string    :single_access_token, :null => false   # optional, see Authlogic::Session::Params
+    t.string    :perishable_token,    :null => false   # optional, see Authlogic::Session::Perishability
+
+    # Magic columns, just like ActiveRecord's created_at and updated_at.
+    # These are automatically maintained by Authlogic if they are present.
+    t.integer   :login_count,         :null => false, :default => 0
+    t.integer   :failed_login_count,  :null => false, :default => 0
+    t.datetime  :last_request_at
+    t.datetime  :current_login_at
+    t.datetime  :last_login_at
+    t.string    :current_login_ip
+    t.string    :last_login_ip
+    
+    t.string    "role"
+    t.datetime  "created_at"
+    t.datetime  "updated_at"
   end
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
