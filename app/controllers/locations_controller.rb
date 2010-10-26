c6389a46db7ce35c8c2af841f8aa41ee731589ee
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-26 00:08:49 -0700

    Category counts show up differently for each location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,11 @@ class LocationsController < ApplicationController
   def show
     @location = Location.find(params[:id])
     @restaurants = @location.restaurants
-    @categories = Category.all
+    @category_counts = {}
+    @restaurants.each do |r|
+      r.categories.each do |c|
+        @category_counts[c.name] = @category_counts[c.name].nil? ? 1 : @category_counts[c.name] + 1
+      end
+    end
   end
 end
