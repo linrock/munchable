daf8f908cda83761d30da62f9e177c80aa358196
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-26 02:09:02 -0700

    Couple of small bugfixes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,9 @@
 class RestaurantsController < ApplicationController
 
-  def index
-=begin
-    @restaurants = []
-    @categories = Category.all(:conditions => {:name => @categories})
-    @categories.each {|c| @restaurants << c.restaurants }
-    @restaurants = @restaurants.flatten.uniq
-=end
-    @location = Location.where(:city => 'San Francisco').first
-    @restaurants = @location.restaurants
-    @categories = Category.all
-  end
-
   def show
     id = params[:id]
     @restaurant = Restaurant.find(id)
+    @location = @restaurant.location
     @comments = MenuComment.where(:restaurant_id => id)
     @coordinates = [@restaurant[:x], @restaurant[:y]]
   end
