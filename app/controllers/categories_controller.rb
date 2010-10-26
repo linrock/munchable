c391232114534a076f47e9ed76af3411e42313d9
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-26 00:42:39 -0700

    Category show view now only shows location restaurants

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,6 @@ class CategoriesController < ApplicationController
   def show
     @location = Location.find(params[:location_id])
     @category = Category.find(params[:id])
+    @restaurants = @category.restaurants.where(:location_id => @location.id)
   end
 end
