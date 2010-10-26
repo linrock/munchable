6bbbdc7d1ba23a36ce165012d1c6d8fc05fcaf5b
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-26 00:38:06 -0700

    Link to correct categories now too... kind of hackish

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,10 +3,14 @@ class LocationsController < ApplicationController
   def show
     @location = Location.find(params[:id])
     @restaurants = @location.restaurants
-    @category_counts = {}
+    @category = {
+      :counts => {},
+      :ids => {}
+    }
     @restaurants.each do |r|
       r.categories.each do |c|
-        @category_counts[c.name] = @category_counts[c.name].nil? ? 1 : @category_counts[c.name] + 1
+        @category[:counts][c.name] = @category[:counts][c.name].nil? ? 1 : @category[:counts][c.name] + 1
+        @category[:ids][c.name] = c.id
       end
     end
   end
