ea81c81eddfe15d29ab36fa79ccee7e990ca958c
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-25 18:18:55 -0700

    Now using Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ActiveRecord::Base
   has_many :restaurants
 
+  @zone = "? > x_lower AND ? < x_upper AND ? > y_lower AND ? < y_upper"
+
+  def self.get_location(x, y)
+    self.where(@zone, x, x, y, y).first
+  end
 end
