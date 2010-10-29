70a60311ad8cc0751b894a2ec6037111cbfd477b
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-28 17:21:40 -0700

    Clean up seeds.rb, move old stuff to seeds.rb.old

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-require RAILS_ROOT + '/db/generator'
-
-
 Location.create([{
   :city => 'San Francisco',
   :state => 'CA',
@@ -31,9 +28,6 @@ Location.create([{
   :y_upper => -122.070637,
 }])
 
-
-fng = FakeNameGenerator.new
-
 places = []
 places.push File.open(RAILS_ROOT + '/db/places/ca_san_francisco.txt').read.split(/\n/)
 places.push File.open(RAILS_ROOT + '/db/places/ca_mountain_view.txt').read.split(/\n/)
@@ -45,35 +39,6 @@ end
 File.open(RAILS_ROOT + '/db/categories/restaurants.txt').read.split(/\n/).each do |c|
   Category.create(:name => c)
 end
-
-=begin
-    r = Restaurant.create({
-      :location_id => Location.get_location(row[4], row[5]).id,
-      :url => row[0],
-      :name => row[1],
-      :categories => row[3],
-      :rating => row[6],
-      :review_count => row[7],
-      :address => row[8],
-      :website => row[9],
-      :hours => row[10],
-      :good_for => row[11],
-      :delivery => row[12],
-      :take_out => row[13],
-      :x => row[4],
-      :y => row[5],
-      :updated_at => row[14]
-    })
-=end
-=begin
-    (rand*10).to_i.times do
-      MenuItem.create(
-        :restaurant_id => r.id,
-        :name => fng.generate_name(row[3]),
-        :price => (rand*20).to_i
-      )
-    end
-=end
 
 def quote (str)
   str.gsub(/\\|'/) { |c| "\\#{c}" }
@@ -91,79 +56,3 @@ end
 
 sql = "INSERT INTO restaurants (location_id, url, name, categories, rating, review_count, address, website, hours, good_for, delivery, take_out, x, y, updated_at) VALUES #{inserts.join(', ')}"
 Restaurant.connection.execute sql
-
-=begin
-m0 = MenuItem.create([{
-  :restaurant_id => 1,
-  :name => 'Submarine Sandwich',
-  :description => 'Sinks like a sub in your stomach',
-  :price => '$5'
-},{
-  :restaurant_id => 1,
-  :name => 'History Hoagie',
-  :description => 'The best hoagie in all of history',
-  :price => '$7'
-}])
-
-c0 = MenuComment.create({
-  :restaurant_id => 1,
-  :menu_item_id => m0[0].id,
-  :content => "dood this sandwich is the shiznit!",
-})
-c1 = MenuComment.create({
-  :restaurant_id => 1,
-  :menu_item_id => m0[0].id,
-  :parent_id => c0.id,
-  :content => "man, never had anything better!"
-})
-c2 = MenuComment.create({
-  :restaurant_id => 1,
-  :menu_item_id => m0[0].id,
-  :parent_id => c1.id,
-  :content => "amen brothah!!"
-})
-
-m1 = MenuItem.create([{
-  :restaurant_id => 2,
-  :name => 'Bacon Cheeseburger',
-  :description => 'Delicious strips of bacon in every bite',
-  :price => '$10'
-},{
-  :restaurant_id => 2,
-  :name => 'Grilled Cheese Sandwich',
-  :description => 'Grilled to perfection',
-  :price => '$8'
-},{
-  :restaurant_id => 2,
-  :name => 'Hickory Smoked Burger',
-  :description => 'Delightful hickory-smoked beef',
-  :price => '$7'
-}])
-c0 = MenuComment.create({
-  :restaurant_id => 2,
-  :menu_item_id => m1[0].id,
-  :content => "hey, where's the downvote button?? this entry doesn't exist!"
-})
-
-m2 = MenuItem.create([{
-  :restaurant_id => 3,
-  :name => 'Petite Reuben',
-  :description => 'Tiny little Reuben, but worth every penny',
-  :price => '$20'
-},{
-  :restaurant_id => 3,
-  :name => 'Petite Smokin Sandwich',
-  :description => 'Smokin good, and to perfection',
-  :price => '$14'
-},{
-  :restaurant_id => 3,
-  :name => 'Tiny Burger',
-  :description => 'Small, but delicious',
-  :price => '$7'
-}])
-c0 = MenuComment.create({
-  :restaurant_id => 3,
-  :menu_item_id => m2[0].id,
-  :content => "kind of expensive for such a tiny reuben, but it aint so bad"
-})
-=end
