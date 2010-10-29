9f1ffa2b23559063739d11464b969c6daf7d07fe
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-29 01:29:34 -0700

    Testing changes
    - Add x,y back to compare queries for PostGIS vs. normal
      - PostGIS much faster for small boundaries (uses GIST index)
      - PostGIS much faster for empty boundaries (uses GIST index)
      - PostGIS slightly slower for large bounderies (table scan)
    - Use Locations to set initial boundaries

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,13 +6,7 @@ class LocationsController < ApplicationController
     @categories = []
     # @location.restaurants.all(:select => 'categories').each do |r|
 
-    Restaurant.where("xy && ?", Polygon.from_coordinates([[
-      [37.6946877, -122.5344078],
-      [37.8116826, -122.5344078],
-      [37.8116826, -122.3550796],
-      [37.6946877, -122.3550796],
-      [37.6946877, -122.5344078]
-    ]], 4326)).all(:select => 'categories').each do |r|
+    Location.find(1).get_restaurants.all(:select => 'categories').each do |r|
       JSON.load(r.categories).each do |c|
         if not @categories.include? c
           @categories.push c
--- app/models/location.rb
+++ app/models/location.rb
@@ -6,4 +6,14 @@ class Location < ActiveRecord::Base
   def self.get_location(x, y)
     self.where(@zone, x, x, y, y).first
   end
+
+  def get_restaurants
+    Restaurant.where("xy && ?", Polygon.from_coordinates([[
+      [x_lower, y_lower],
+      [x_upper, y_lower],
+      [x_upper, y_upper],
+      [x_upper, y_upper],
+      [x_lower, y_lower]
+    ]], 4326))
+  end
 end
