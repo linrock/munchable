91b06c46ccad6309f59c02ed68c8e93da22fc8e1
Linmiao Xu <linmiao.xu@gmail.com>
2010-11-05 03:14:38 -0700

    Restaurant adding now works

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,10 @@ class ListsController < ApplicationController
     rid = params[:rid]
     name = params[:q]
 
+    list = List.where(:group_id => params[:group_id]).first
+    list.restaurants << Restaurant.find(params[:rid])
+    list.save
+
     render :json => ['asdf']
   end
 end
