e7a978dafb35d7a47722b78847342d8ebae1ec96
Linmiao Xu <linmiao.xu@gmail.com>
2010-11-06 05:03:39 -0700

    Minor authorization additions.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,9 @@ class ProfilesController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
+    if not can? :manage, @user
+      flash[:error] = "Hey, you're not supposed to be here!"
+    end
   end
 
   def edit
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, User, :id => user.id
-    can :manage, :all
+    if user
+      can :manage, User, :id => user.id
+      can :manage, [Profile, Group, Listing], :user_id => user.id
+    end
+    can :create, [User, Group, Listing]
   end
 end
