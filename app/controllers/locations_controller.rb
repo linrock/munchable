60ab7c517bac76f478af6c85e6301173b469f625
Linmiao Xu <linmiao.xu@gmail.com>
2010-11-07 01:15:16 -0800

    Use all categories... not just local categories

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,16 +2,6 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.find(params[:id])
-    @categories = []
-    Restaurant.within_bounds(@location.bounds).all(:select => 'categories').each do |r|
-      JSON.load(r.categories).each do |c|
-        if not @categories.include? c
-          @categories.push c
-        end
-      end
-    end
-    @categories = @categories.to_json
-
-    # @categories = Category.all.collect {|c| c.name }
+    @categories = Category.all.collect {|c| c.name }
   end
 end
